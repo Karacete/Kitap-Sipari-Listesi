17592ea5360332cc2db7e84e4ebdd734d3ece24b
Muhammed Emin Karaçete <muhammed.karacete@ogr.sakarya.edu.tr>
2023-03-31 22:31:31 +0300

    Kitap Sipariş Listesi

---
+++ KitapSiparisListesiOdev/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KitapSiparisListesiOdev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (checkedListBox1.SelectedItem == null)
+            {
+                button1.Enabled = false;
+            }
+            else if (checkedListBox1.SelectedItem != null)
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                listBox1.Items.Add(checkedListBox1.CheckedItems[i]);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
